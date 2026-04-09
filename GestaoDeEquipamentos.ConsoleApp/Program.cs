6d94b18ea323098db41595fd63872816c292aa72
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-04-09 16:25:20 -0300

    Adicionada exclusão de Equipamentos

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using GestaoDeEquipamentos.ConsoleApp;
 
 Equipamento?[] equipamentos = new Equipamento[100];
@@ -19,7 +20,7 @@ equipamentos[0] = equipamentoTeste;
 
 while (true)
 {
-    // Console.Clear();
+    Console.Clear();
     Console.WriteLine("---------------------------------");
     Console.WriteLine("Gestão de Equipamentos");
     Console.WriteLine("---------------------------------");
@@ -40,7 +41,7 @@ while (true)
 
     if (opcaoMenu == "1")
     {
-        // Console.Clear();
+        Console.Clear();
         Console.WriteLine("---------------------------------");
         Console.WriteLine("Gestão de Equipamentos");
         Console.WriteLine("---------------------------------");
@@ -111,7 +112,7 @@ while (true)
             possa editar todos os campos.
                 • Deve ter os mesmos critérios que o Requisito 1.
         */
-        // Console.Clear();
+        Console.Clear();
         Console.WriteLine("---------------------------------");
         Console.WriteLine("Gestão de Equipamentos");
         Console.WriteLine("---------------------------------");
@@ -119,7 +120,6 @@ while (true)
         Console.WriteLine("---------------------------------");
 
         // 1. Perguntar qual equipamento o usuário quer editar
-
         Console.WriteLine(
             "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
             "Id", "Nome", "Fabricante", "Preço de Aquisição", "Data de Fabricação"
@@ -137,6 +137,8 @@ while (true)
                 e.id, e.nome, e.fabricante, e.precoAquisicao.ToString("C2"), e.dataFabricacao.ToShortDateString()
             );
         }
+
+        Console.WriteLine("---------------------------------");
 
         string? idSelecionado;
 
@@ -226,7 +228,78 @@ while (true)
 
     else if (opcaoMenu == "3")
     {
+        Console.Clear();
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Gestão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Exclusão de Equipamento");
+        Console.WriteLine("---------------------------------");
 
+        // 1. Perguntar qual equipamento o usuário quer excluir
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+            "Id", "Nome", "Fabricante", "Preço de Aquisição", "Data de Fabricação"
+        );
+
+        for (int i = 0; i < equipamentos.Length; i++)
+        {
+            Equipamento? e = equipamentos[i];
+
+            if (e == null) // null guard/check
+                continue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+                e.id, e.nome, e.fabricante, e.precoAquisicao.ToString("C2"), e.dataFabricacao.ToShortDateString()
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+
+        string? idSelecionado;
+
+        do
+        {
+            Console.Write("Digite o id do equipamento que deseja excluir: ");
+            idSelecionado = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(idSelecionado) && idSelecionado.Length == 7)
+                break;
+        } while (true);
+
+        // 2. Buscar o espaço em que o equipamento selecionado está armazenado
+        bool equipamentoExcluido = false;
+
+        for (int i = 0; i < equipamentos.Length; i++)
+        {
+            Equipamento? e = equipamentos[i];
+
+            if (e == null)
+                continue;
+
+            if (e.id == idSelecionado)
+            {
+                equipamentos[i] = null;
+                equipamentoExcluido = true;
+                break;
+            }
+        }
+
+        if (equipamentoExcluido)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"O registro \"{idSelecionado}\" foi excluído com sucesso.");
+            Console.WriteLine("---------------------------------");
+        }
+        else
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"Não foi possível encontar o registro \"{idSelecionado}\".");
+            Console.WriteLine("---------------------------------");
+        }
+
+        Console.Write("Digite ENTER para continuar...");
+        Console.ReadLine();
     }
 
     else if (opcaoMenu == "4")
